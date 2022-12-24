36978295e99d589af6b8e04e9ec1f960e8a46af7
AleksaPol <113082742+AleksaPol@users.noreply.github.com>
2022-12-24 20:00:46 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import math
+import art
+# gcd(a, b): получение наибольшего общего делителя чисел a и b;
+a = 50
+b = 35
+print('Наибольший общий делитель a и b: ' +
+      art.text2art(str(math.gcd(a, b))))
